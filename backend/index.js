8f31872a1b90281a75cdd9533b1ff8f77a1a0ac7
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-10-22 10:50:07 -0500

    category back end finalized

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 
-//Utiles
+//U    
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+
 dotenv.config()
 
 const port = process.env.PORT || 5000
@@ -19,6 +21,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 app.use('/api/users',userRoutes)
+app.use('/api/category', categoryRoutes)
 
   app.get('/',(req,res)=>{
     res.json('hello home')

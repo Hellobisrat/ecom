b640dadecb3342449373eeca32454c3633306d05
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-11-15 12:31:03 -0600

    product pages added

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,39 @@
+// packages
+import path from "path";
 import express from "express";
-import path from 'path';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-//U    
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-dotenv.config()
+// Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-const port = process.env.PORT || 5000
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/api/users',userRoutes)
-app.use('/api/category', categoryRoutes)
-app.use('/api/products',productRoutes)
-  app.get('/',(req,res)=>{
-    res.json('hello home')
-  })
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.listen(port,()=>{
-  console.log(`server running on port:${port}`)
-})
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));

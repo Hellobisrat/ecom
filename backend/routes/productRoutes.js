7696f307ab76041ad3616ba1f52579f89d8090e2
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-11-15 10:06:52 -0600

    routes added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,51 @@
 import express from "express";
-import formidable from 'express-formidable';
+import formidable from "express-formidable";
+const router = express.Router();
 
-const router = express.Router()
+// controllers
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
+  filterProducts,
+} from "../controllers/productController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
 
-import { addProduct ,updateProductDetails,removeProduct,fetchProducts,fetchProductById,fetchAllProducts,addProductReview,fetchTopProducts,fetchNewProducts } from "../controllers/productController.js";
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
 
-import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
-import checkId from '../middlewares/checkId.js'
+router.route("/allproducts").get(fetchAllProducts);
+router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
 
+router.get("/top", fetchTopProducts);
+router.get("/new", fetchNewProducts);
 
-router.route('/').get(fetchProducts).post(authenticate,authorizeAdmin,formidable(),addProduct)
-router.route('/allproducts').get(fetchAllProducts)
-router.route('/:id/reviews').post(authenticate,authorizeAdmin,addProductReview)
-router.get('/top',fetchTopProducts)
-router.get('/new',fetchNewProducts)
-router.route('/:id')
-.get(fetchProductById)
-.put(authenticate,authorizeAdmin,formidable(),updateProductDetails)
-.delete(authenticate,authorizeAdmin,removeProduct)
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
 
-
-
-
-
-
-
-
-
-
-
-
+router.route("/filtered-products").post(filterProducts);
 
 export default router;
+
+
+
+
+
+
+
+
+
+
+
